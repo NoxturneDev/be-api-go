375a7d2090a36dc672a1c238760fab7a224450b2
Galih Adhi Kusuma <noxturne.production@gmail.com>
2024-06-15 11:44:39 +0700

    feat: api routes for chat rooms model

--- app/routes.go
+++ app/routes.go
@@ -9,10 +9,15 @@ import (
 
 func Routes(app *fiber.App) {
 	app.Get("/api/users", handler.GetUsers)
-	app.Get("/api/user/:id", handler.GetUser)
-	app.Post("/api/user", handler.CreateUser)
-	app.Put("/api/user", handler.UpdateUser)
-	app.Delete("/api/user/:id", handler.DeleteUser)
+	app.Get("/api/users/:id", handler.GetUser)
+	app.Post("/api/users", handler.CreateUser)
+	app.Put("/api/users", handler.UpdateUser)
+	app.Delete("/api/users/:id", handler.DeleteUser)
+
+	app.Get("/api/chatrooms", handler.GetChatRooms)
+	app.Get("/api/chatrooms/:id", handler.GetChatRoom)
+	app.Get("/api/chatrooms/seller/:seller_id", handler.GetChatRoomBySellerId)
+	app.Post("/api/chatrooms", handler.CreateChatRoom)
 
 	//app.Get("/api/ai/test", handler.TestAIconnection)
 
