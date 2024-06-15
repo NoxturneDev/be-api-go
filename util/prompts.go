10ad79f297e2f52c998017de3bf04193dca9015f
Galih Adhi Kusuma <noxturne.production@gmail.com>
2024-06-15 21:18:05 +0700

    update

--- util/prompts.go
+++ util/prompts.go
@@ -10,28 +10,28 @@ func CreateSuggestionAIPrompt(parameter string) string {
 }
 
 func CreateCustomerSentimentAnalysisPrompt(parameter string) string {
-	promptDesign := "Aku ingin meminta bantuanmu untuk membuatkan Analisa Mood pelangganku dari chat ini. Pertimbangkan aspek kepuasan, kebahagiaan, dan aspek aspek sentimen lainya. berapapun data chat yang diberikan, lansung saja berikan hasil penilaian analisa sentimen customernya ya. kamu bisa menggunakan skala apapun yang kamu mau. berikut adalah data chatnya: [chat]"
+	promptDesign := "Aku ingin meminta bantuanmu untuk membuatkan Analisa Mood pelangganku dari chat ini. Pertimbangkan aspek kepuasan, kebahagiaan, dan aspek aspek sentimen lainya. berapapun data chat yang diberikan, lansung saja berikan hasil penilaian analisa sentimen customernya ya. kamu bisa menggunakan skala apapun yang kamu mau. berikut adalah data chatnya: [chat]. NOTE: Jawab dalam bahasa yang digunakan dalam chat tersebut yaa!"
 
 	prompt := strings.Replace(promptDesign, "[chat]", parameter, -1)
 	return prompt
 }
 
 func TranslateToLanguagePrompt(parameter string) string {
-	promptDesign := "Kamu adalah asisten ku, dan Aku adalah seorang Customer service. Aku ingin meminta bantuanmu untuk menerjemahkan text ini ke dalam bahasa [language]. berikut adalah data textnya:"
+	promptDesign := "Kamu adalah asisten ku, dan Aku adalah seorang Customer service. Aku ingin meminta bantuanmu untuk menerjemahkan text ini ke dalam bahasa [language]. berikut adalah data textnya"
 
 	prompt := strings.Replace(promptDesign, "[language]", parameter, -1)
 	return prompt
 }
 
 func CreateSummaryAiPrompt(parameter string) string {
-	promptDesign := "Kamu adalah asisten ku, dan Aku adalah seorang Customer service. Aku ingin meminta bantuanmu untuk membuatkan Summary dari data-data chat yang sudah ada dari user ini. Kamu hanya perlu mengambil inti dari pertanyaan tersebut lalu buatkan listnya untuku. berikut adalah data chatnya: [chat]"
+	promptDesign := "Aku adalah seorang Arsitek dan aku membutuhkan bantuanmu untuk mencatat rangkuman dari permintaan pelangganku ini. Kamu hanya perlu mengambil inti dari pertanyaan tersebut lalu buatkan rangkumanya untuku yaa. tidak usah berikan tanggapan mu, langsung saja buatkan rangkumanya dan detail yang diinginkan oleh pelangganku ya. buatkan sedetail mungkin yaa. jangan lewatkan sedikitpun karena ini penting sekali untuk ku. berikut adalah data chatnya: [chat]. NOTE: buatkan dalam bentuk bullet points ya. dan jawab dengan bahasa yang digunakan dalam chat tersebut ya!"
 
 	prompt := strings.Replace(promptDesign, "[chat]", parameter, -1)
 	return prompt
 }
 
 func CreateMockUserChatting(parameter string) string {
-	promptDesign := "Kamu adalah pelanggan ku yang ingin membeli sebuah furnitur ataupun sedang mencari seorang Arsitek dan penyedia jasa Design Interior. dan sekarang kamu sedang berkonsultasi dengan aku, jawab pertanyaan ku seolah-olah kamu memang benar-benar pelanggan. Buat respon mu se variatif mungkin dan selayaknya pelanggan asli. Jika kamu sudah mendapat lebih dari 4 pertanyaan atau kamu merasa cukup. kamu bisa mengakhiri percakapan dengan membuat statement bahwa kamnu tertarik ya. atau jika harga dari yang ditawarkan menurut kamu sudah oke, maka kamu juga bisa memberhentikan percakapan. berikut adalah pertanyaan ku: [chat]. Note: kamu bisa bebas menentukan mood kamu, baik dari senang hingga kesal. agar chat berkesan variatif"
+	promptDesign := "Kamu adalah pelanggan ku yang ingin membeli sebuah furnitur ataupun sedang mencari seorang Arsitek dan penyedia jasa Design Interior. dan sekarang kamu sedang berkonsultasi dengan aku, jawab pertanyaan ku seolah-olah kamu memang benar-benar pelanggan. Buat respon mu se variatif mungkin dan selayaknya pelanggan asli. Jika kamu sudah mendapat lebih dari 4 pertanyaan atau kamu merasa cukup. kamu bisa mengakhiri percakapan dengan membuat statement bahwa kamnu tertarik ya. atau jika harga dari yang ditawarkan menurut kamu sudah oke, maka kamu juga bisa memberhentikan percakapan. berikut adalah pertanyaan ku: [chat]. Note: kamu bisa bebas menentukan mood kamu, baik dari senang hingga kesal. agar chat berkesan variatif. NOTE: Jawab dengan bahasa yang aku gunakan ya"
 
 	prompt := strings.Replace(promptDesign, "[chat]", parameter, -1)
 	return prompt
